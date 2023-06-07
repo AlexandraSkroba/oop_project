828f3c118c00d3c823d7e0d14ea685438806c068
AlexandraSkroba <aleksa1012016@gmail.com>
2023-06-07 19:14:08 +0300

    add Item and User models, update gitignore

---
+++ Planner.Logic/ItemModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Planner.Logic
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string Priority { get; set; }
+        public bool IsCompleted { get; set; }
+        public UserModel User { get; set; }
+        public int? UserId { get; set; }
+        public IEnumerable<string> States { get; }
+        public IEnumerable<string> Priorities { get; }
+
+        public ItemModel()
+        {
+            States = new List<string>()
+            {
+                "Show Active",
+                "Show Completed"
+            };
+            Priorities = new List<string>()
+            {
+                "High",
+                "Medium",
+                "Low"
+            };
+        }
+    }
+}
---
+++ Planner.Logic/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Planner.Logic
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public ICollection<ItemModel> Items { get; set; }
+    }
+}
